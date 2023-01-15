46a18ff6f931854df4334af25d5463874c8359de
Geen-19 <angelinedias53@gmail.com>
2023-01-15 14:33:12 +0530

    Stack, Tab, Nav Navigators are implemented using 3 screens

--- App.js
+++ App.js
@@ -4,22 +4,17 @@ import {Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const HomeScreen = ({navigation}) => { 
   return (
     <View style = {styles.centering} >
-      <Text>Hello World</Text>
+      <Text>Home Screen</Text>
       <Button
         title="Go to details"
         onPress={() =>
-          navigation.push('Details', { name: 'Custom profile header' }, 
-          {
-          itemId: '69',
-          otherParam : 'I am 6ft',
-          fact: 'Geen',
-          cool : 'React'})
+          navigation.navigate('Details')
         }
       />
-      <Text>Count: {count}</Text>
      <Button
         onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
@@ -30,38 +25,47 @@ const HomeScreen = ({navigation}) => {
 function NotificationScreen({navigation}) {
   return(
     <View style = {styles.centering}>
-      <Button onPress={() => navigation.goBack()}
-      title = 'Go back home'/>
+      <Text>Notification Screen</Text>
     </View>
   );
 }
 function DetailsScreen({route , navigation}){
-  const {itemId, otherParam, imp, cool} = route.params;
   return(
-    <View style = {styles.centering}>
-        <Text>Details</Text>
-        <Text>ItemId: {JSON.stringify(itemId)} </Text>
-        <Text>Param: {JSON.stringify(otherParam)}</Text>
-        <Text>Fact: {JSON.stringify(imp)} </Text>
-        <Text>Author Name: {JSON.stringify(cool)}</Text>
-        <Button 
-        title='Go Back'
-        onPress={() => navigation.navigate('Home')}/>
-      
+    <View style = {styles.centering}>  
+        <Text>Details Screen</Text>  
     </View>
   );
 }
-
+function TabNavImplementation () {
+  return(
+    <Tab.Navigator >
+      <Tab.Screen name = 'Home' component={HomeScreen} options = {{headerShown: false}} />
+      <Tab.Screen name = 'Notification' component={NotificationScreen} options = {{headerShown: false}}/>
+      <Tab.Screen name = 'Details' component = {DetailsScreen} options = {{headerShown: false}}/>
+    </Tab.Navigator>
+  )
+}
+function StackNavImplementation () {
+  return(
+    <Stack.Navigator> 
+      <Stack.Navigator name = 'Home' component = {HomeScreen}/>
+      <Stack.Navigator name = 'Details' component = {DetailsScreen}/>
+      <Stack.Navigator name = 'Notification' component = {NotificationScreen}/>
+    </Stack.Navigator>
+  )
+}
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 function App() {
   return(
   <NavigationContainer>
     <Drawer.Navigator initialRouteName='Home'>
-      <Drawer.Screen name='Home' component={HomeScreen} />
+      <Drawer.Screen name='HomeScreen' component={TabNavImplementation} />
       <Drawer.Screen name='Notifications' component={NotificationScreen} />
+      <Drawer.Screen name='Detail' component={StackNavImplementation}/>
     </Drawer.Navigator>
-    <Stack.Navigator 
+    {/* <Stack.Navigator 
     screenOptions={{title: 'My Home',
     headerStyle: {
       backgroundColor: '#f4511e',
@@ -81,14 +85,14 @@ function App() {
     />
     <Stack.Screen name='Details' component={DetailsScreen} initialParams = {{itemId: 54}} 
     options = {({route}) => ({title: route.params.name})}/>
-    </Stack.Navigator>
+    </Stack.Navigator> */}
   </NavigationContainer>
   );
 }
 // Logo for header
 const styles = StyleSheet.create({
   centering: {
-    flex: flex-start,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
